d4a02f0cc78e832fb06c8b6039fea4118864d035
mikejguo290 <mikejguo290@gmail.com>
2021-09-21 15:29:09 +0100

    add get previous and get next image buttons.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,11 +4,14 @@ import TodosList from './TodosList';
 import Quote from './Quote';
 
 function Homepage () {
+    /* homepage will have to have images state */
     return (
         <section className="homepage">
             <Weather />
             <InputTodos />
             <TodosList />
+            <button>Previous Image</button> 
+            <button>Next Image</button> 
             <br />
             <TodosList />
             <Quote />
