a5fe05be402494b755aecd155280a481026b46ce
mikejguo290 <mikejguo290@gmail.com>
2021-09-21 17:13:41 +0100

    render todoList with mock data and pass it as props to rendered Todo elements.

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,12 +1,28 @@
 import Todo from './Todo';
 
-function TodosList(){
-    const numbers = [1,2,3]
-    const listItems = numbers.map(()=><li><Todo/></li>);
+function TodosList(props){
+
+    const todos = [
+        {id:1, text:'play guitar', isCompleted: false},
+        {id:2, text:'meal plan', isCompleted: false},
+        {id:3, text:'read', isCompleted: false},
+        {id:1, text:'draw', isCompleted: true},
+        {id:2, text:'learn Spanish', isCompleted: true},
+        {id:3, text:'gym', isCompleted: true},
+    ];
+
+    // either get all todos and filter later, or useSelectors conditionally 
+    const filteredItems = todos.filter(( todo )=> props.isCompleted === todo.isCompleted).map((todo)=>{
+        const {id, text, isCompleted } = todo;
+        return (
+            <li key={id}><Todo id={id} text={text} isCompleted={isCompleted}/></li>
+        )
+    });
+
     return (
         <ul> 
             {/* replace with dynamically generated list when there is real data */}
-            {listItems}
+            {filteredItems}
         </ul>
     );
 }
