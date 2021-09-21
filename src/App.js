d4c62c53fb6d8c2623ea5612eac94d478478f669
mikejguo290 <mikejguo290@gmail.com>
2021-09-21 13:17:07 +0100

    rename the home page component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Homepage from './components/HomePage';
+import Homepage from './components/Homepage';
 import './App.css';
 
 function App() {
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,16 +3,17 @@ import InputTodos from './InputTodos';
 import TodosList from './TodosList';
 import Quote from './Quote';
 
-function HomePage () {
+function Homepage () {
     return (
         <section className="homepage">
             <Weather />
             <InputTodos />
             <TodosList />
+            <br />
             <TodosList />
             <Quote />
         </section>
     );
 }
 
-export default HomePage; 
+export default Homepage; 
