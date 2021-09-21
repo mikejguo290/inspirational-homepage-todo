7b9caa72586818628a8a42edf8ccf678772f784c
mikejguo290 <mikejguo290@gmail.com>
2021-09-21 23:15:42 +0100

    set up homepage with inline style which includes a background image.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,8 +5,15 @@ import Quote from './Quote';
 
 function Homepage () {
     /* homepage will have to have images state */
+    const imageURL = 'https://source.unsplash.com/random';
+    const background = `url(${imageURL})`;
+    let style= {
+        backgroundImage: background,
+        backgroundSize:'cover',
+    }
+
     return (
-        <section className="homepage">
+        <section className="homepage" style={style}>
             <Weather />
             <InputTodos />
             <TodosList isCompleted={false} />
