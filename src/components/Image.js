a72dc1aac9aab9bb5bf132cdbc85e67340a03f58
mikejguo290 <mikejguo290@gmail.com>
2021-09-21 17:57:34 +0100

    add an element that will render the fetched images in its background.

---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+
+function Image (){
+    //element to take up 100% width of parent. other elements to have show over it because of higher z-index 
+    return (
+        <div class = "image">
+            <h3>Some image in the background</h3>
+        </div>
+    );
+}
+
+export default Image;
