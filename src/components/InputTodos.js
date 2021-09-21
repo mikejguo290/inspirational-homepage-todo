4517248f6c830fe9952645f388f87672f3829f54
mikejguo290 <mikejguo290@gmail.com>
2021-09-21 13:13:59 +0100

    move input-dodos input to within a form.

--- src/components/InputTodos.js
+++ src/components/InputTodos.js
@@ -2,7 +2,12 @@
 function InputTodos(){
     return (
         <section className = "add-todo">
-            <input id="input-todos">What's on your mind today?</input>
+            <form >
+                <label for="input-todos">
+                    {/* specify input's value to control the element */}
+                    <input id="input-todos" type ='text' placeholder="What's on your mind?"/>
+                </label>
+            </form>
         </section>
     );
 }
