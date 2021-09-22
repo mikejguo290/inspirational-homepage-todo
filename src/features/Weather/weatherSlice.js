7ffbcc865fe38254a8aa5158b76689040103b696
mikejguo290 <mikejguo290@gmail.com>
2021-09-22 14:00:00 +0100

    create slice for Weather.

---
+++ src/features/Weather/weatherSlice.js
@@ -0,0 +1,22 @@
+import createSlice from 'reduxjs/toolkit';
+
+const options = {
+    name: 'Weather',
+    initialState: { weather:{} },
+    reducers: {
+        setWeather:(state, action) => {
+            // set state weather with action.payload
+            const { temperature, description, weatherIcon } = action.payload;
+            state.weather = {
+                temperature: temperature,
+                description: description,
+                weatherIcon: weatherIcon,
+            }
+        }
+    }
+}
+
+const weatherSlice = createSlice(options);
+
+export const { setWeather } = weatherSlice.actions;
+export default weatherSlice.reducer;
