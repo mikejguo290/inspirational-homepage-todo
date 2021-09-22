52773eeaaf6a1d6b7b9fc374a7c55cc924bd2d8f
mikejguo290 <mikejguo290@gmail.com>
2021-09-22 22:35:36 +0100

    add imageReducer to rootReducers in store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todosReducer from '../features/Todos/todosSlice';
 import weatherReducer from '../features/Weather/weatherSlice';
+import quoteReducer from '../features/Quote/quoteSlice';
+import imageReducer from '../features/Image/imageSlice';
 
 const rootReducer = {
+    image: imageReducer,
+    quote: quoteReducer,
     todos: todosReducer,
     weather: weatherReducer,
 }
