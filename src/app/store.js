a739e6c63ed24e300ce2d545659e85edf9fbb601
mikejguo290 <mikejguo290@gmail.com>
2021-09-22 14:02:08 +0100

    configure store with weatherReducer - add weatherReducer to rootReducer.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import configureStore from '@reduxjs/toolkit';
 import todosReducer from '../features/Todos/todosSlice';
+import weatherReducer from '../features/Weather/weatherSlice';
 
 const rootReducer = {
     todos: todosReducer,
+    weather: weatherReducer,
 }
 
 const store = configureStore(rootReducer);
+
+export default store;
