82ddce530ba780348c7c5b03c492a583faaa4b91
mikejguo290 <mikejguo290@gmail.com>
2021-09-22 16:12:55 +0100

    create handlers for todo deletion and completion, with action creators and useDispatch hook.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,26 @@
+import { useDispatch } from 'react-redux';
+import { deleteTodo, completeTodo } from '../features/Todos/todosSlice';
 
 function Todo(props){
-    const { text, isComplete } = props;
+    const { id, text, isComplete } = props;
     const completionClass = isComplete? 'todo completed': 'todo current';
+    
+    const dispatch = useDispatch();
+
+    const handleDelete=()=>{
+        dispatch(deleteTodo({id:id}));
+    }
+
+    const handleComplete=()=>{
+        dispatch(completeTodo({id:id}));
+    }
+
     return (
         <article className={completionClass}>
             {/* replace with actual data*/}
             <h3>{text}</h3>
-            <button>Remove</button>
-            <button>Done</button>
+            <button onClick={handleDelete}>Remove</button>
+            <button onClick={handleComplete}>Done</button>
         </article>
     )
 }
