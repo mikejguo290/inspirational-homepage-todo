be2f2cd00c50ac7a23adc0051d54745991a5afd0
mikejguo290 <mikejguo290@gmail.com>
2021-09-23 20:46:10 +0100

    use createAsyncThunk to create thunk action creator for making calls to quotes api and setting action.payload to response.

--- src/features/Quote/quoteSlice.js
+++ src/features/Quote/quoteSlice.js
@@ -1,16 +1,49 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import quotesAPI from '../../api/quotesAPI';
 
+export const fetchQuote = createAsyncThunk(
+    'quote/fetchQuote', // name the action types created 
+     async(arg, thunkAPI)=>{ // async call back to run 
+        const fetchedQuote = await quotesAPI.fetchQuoteOfTheDay();
+        return fetchedQuote;
+     }
+);
 
 const options = {
     name: 'quote',
     initialState:{ quote: {
         text:'Beep … beep … beep.',
         author:'Sputnik',
-    }},
+        },
+        // states created to track loading status
+        isLoading:false, 
+        hasError:false,
+    },
+
     reducers: {
         
     },
-    extraReducers:{}
+    extraReducers:{
+        [fetchQuote.fulfilled]:(state,action)=>{
+            state.isLoading = false;
+            state.hasError = false;
+            // on fufilled, dispatch action to store to update state
+            const { text, author } = action.payload;
+            state.quote = {
+                text:text,
+                author:author
+            }
+        },
+
+        [fetchQuote.pending]:(state)=>{
+            state.isLoading = true;
+            state.hasError = false;
+        },
+        [fetchQuote.rejected]:(state)=>{
+            state.isLoading = false;
+            state.hasError = true;
+        },
+    }
 }
 
 const quoteSlice = createSlice(options);
