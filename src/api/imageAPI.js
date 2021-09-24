38e9897fd9acc613302fba3e73d35af56da59510
mikejguo290 <mikejguo290@gmail.com>
2021-09-24 23:59:54 +0100

    fetchImage to accept an page arg which affect the page of results to fetch and also structure the returned object.

--- src/api/imageAPI.js
+++ src/api/imageAPI.js
@@ -1,9 +1,9 @@
 // using Unsplash API to fetch image urls.
 const accessKey = 'F3IT-ZZ3z3WQA1-WmIB5-M9M3cQ1VJkETxlJdZiqZVs';
 const imageAPI = {
-    async fetchImage(){
+    async fetchImage(pageToFetch){
         // search unsplash api with query 'mountain'
-        const url = `https://api.unsplash.com/search/photos?client_id=${accessKey}&query=mountain&page=3`;
+        const url = `https://api.unsplash.com/search/photos?client_id=${accessKey}&query=mountain&page=${pageToFetch}`;
         try{
             const response = await fetch(url, {
                 method: 'GET',
@@ -16,7 +16,26 @@ const imageAPI = {
                 const jsonResponse = await response.json();
                 // create object to return, this will become action.payload
                 console.log(jsonResponse);
-                return jsonResponse;
+                // 1. define general structure of slice , e.g. images go into array of objects. 
+                // 2. return objects with properties set with jsonResponse.  
+                const images = jsonResponse.results.map(image => {
+                    const { id, created_at, description, alt_description, likes, tags, urls, } = image;
+                    return {
+                        id:id,
+                        created_at:created_at,
+                        description:description, 
+                        alt_description: alt_description,
+                        likes: likes,
+                        tags: tags, // an array of tag objects
+                        urls: urls // object with types of images as properties and their urls as values. 
+                    }
+                })
+                const totalPages = jsonResponse.total_pages;
+                const data = {
+                    images: images,
+                    totalPages:totalPages,
+                }
+                return data;
             }else{
                 throw new Error('Request has failed!')
             }
