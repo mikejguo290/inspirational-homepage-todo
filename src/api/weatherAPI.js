a4700f73e475708398a7e248a7f44abe83552bbb
mikejguo290 <mikejguo290@gmail.com>
2021-09-24 11:52:38 +0100

    prepend url to cors-anywhere to the url used for call to weatherAPI.

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -3,22 +3,27 @@ const apiKey = '68f0e305a7c7de98fc11d2bf9d32994c';
 const weatherAPI = {
     async fetchWeather(){
         try{
-            const url = `api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=${apiKey}`
-            const response = await fetch(url);
+            const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=${apiKey}`
+            const response = await fetch(url,{
+                method:'GET',
+                headers: {
+                    "Content-type":"application/json",
+                }
+            });
+
             if(response.ok){
                 const jsonResponse = await response.json();
                 
                 // create data object to return 
-                const { main, weather } = jsonResponse;
                 // format weather icon url with the weather.icon value from the response. 
-                const weatherIcon = `http://openweathermap.org/img/w/${weather.icon}.png`;
+                const weatherIcon = `http://openweathermap.org/img/w/${jsonResponse.weather[0].icon}.png`;
                 
                 const weatherData = {
-                    temperature: main.temp,
-                    description: weather.description,
+                    temperature: jsonResponse.main.temp,
+                    description: jsonResponse.weather[0].description,
                     weatherIcon: weatherIcon, 
                 }
-
+        
                 return weatherData;
             }else{
                 throw new Error('Request has failed!');
