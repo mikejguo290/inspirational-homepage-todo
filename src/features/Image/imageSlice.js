25d16e40720caad888a39564ed059c7400f82491
mikejguo290 <mikejguo290@gmail.com>
2021-09-25 23:43:42 +0100

    start to modify fetchImage asyncThunkActionCreator to dispatch additional actions.

--- src/features/Image/imageSlice.js
+++ src/features/Image/imageSlice.js
@@ -1,10 +1,37 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 import imageAPI from '../../api/imageAPI';
 
 export const fetchImage = createAsyncThunk(
     'image/fetchImage',
-    async(page)=>{
-        const fetchedImageData = await imageAPI.fetchImage(page);  
+    async(newPage, thunkAPI)=>{
+        const fetchedImageData = await imageAPI.fetchImage(newPage);  
+
+        const dispatch = useDispatch();
+        const store = thunkAPI.getState();
+        const currentPage = store.image.page;
+
+        const maxIndex = fetchedImageData.images.length-1;
+
+        // set imageIndex and also updates page prior to populating store with new image objects. 
+        if (newPage < currentPage){
+            // newPage is previous page
+            // dispatch action to set index to last index of previous page's images, and also set page
+            const payload = {
+                imageIndex: maxIndex,
+                page: newPage,
+            }
+            dispatch(setIndexAndPage(payload)); // you can dispatch an action creator defined in the same file lower down!
+
+        }else if (newPage > currentPage){
+            // dispatch action to set index to index 0 (first image of new page), and also set page
+            const payload = {
+                imageIndex: 0,
+                page: newPage,
+            }
+            dispatch(setIndexAndPage(payload));
+        }
+        
         return fetchedImageData; 
         // action.payload is going to fetch more than a list of image objs. 
     }
@@ -17,7 +44,7 @@ const options = {
         // 'https://images.unsplash.com/photo-1631551831518-b5b32d35f248'
         images : [], // array of image objects - [ {urls: {regular: 'url to use for background'}} ]
 
-        pageToFetch: 2, // page fetched by api is 1 by default, if nothing is specified. starting on page 2 is better!
+        page: 2, // page fetched by api is 1 by default, if nothing is specified. starting on page 2 is better!
         imageIndex: 0, // index of image url to get. 
         totalPages: 0, // total pages of results that can be fetched from query. pageToFetch cannot exceed this. 
 
@@ -32,6 +59,10 @@ const options = {
         getPreviousImage : (state) => {
             // decrease imageIndex by 1.
             state.imageIndex -=1;
+        },
+        setIndexAndPage : (state, action)=>{
+            state.imageIndex = action.payload.imageIndex;
+            state.page = action.payload.page;
         }
     },
     extraReducers:{
@@ -63,10 +94,10 @@ const imageSlice = createSlice(options);
 
 export const selectImageIndex = state => state.image.imageIndex;
 export const selectImages = state => state.image.images;
-export const selectPageToFetch = state => state.image.pageToFetch;
+export const selectPage = state => state.image.page;
 export const selectTotalPages = state => state.image.totalPages;
 export const selectLoadingStatus = state => state.image.isLoading;
 export const selectErrorStatus = state => state.image.isError;
 
-export const { getNextImage, getPreviousImage } = imageSlice.actions;
+export const { getNextImage, getPreviousImage, setIndexAndPage } = imageSlice.actions;
 export default imageSlice.reducer;
