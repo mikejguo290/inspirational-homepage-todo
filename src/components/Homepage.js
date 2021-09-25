f830b7b4351849ef1245b6d8cac3a03a1598cd12
mikejguo290 <mikejguo290@gmail.com>
2021-09-25 17:20:59 +0100

    add logic to set background image url only when image is retrieved from slice.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,10 +5,14 @@ import InputTodos from './InputTodos';
 
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectImage, selectPageToFetch, fetchImage } from '../features/Image/imageSlice'
+import { selectImage, selectPageToFetch, fetchImage, selectLoadingStatus, selectErrorStatus } from '../features/Image/imageSlice'
 
 function Homepage () {
-    /* homepage will have to have images state */
+    /* 
+    homepage renders for the fist time without data, after rendering, the side effect takes place. dispatch thunk
+    action creator to fetchImage. the image data is fetched from imageAPI and the imageSlice is updated. Homepage is rerendered due
+    to its use of selectors exported from imageSlice. 
+    */
     const dispatch = useDispatch();
     const page = useSelector(selectPageToFetch);
     useEffect(()=>{
@@ -16,11 +20,21 @@ function Homepage () {
         dispatch(fetchImage(page));
     },[dispatch, page])
 
-    const image = useSelector(selectImage);
-    const background = `url(${image.urls.regular})`;
-    let style= {
-        backgroundImage: background,
+    // if no image is fetched - const image = useSelector(selectImage); => undefined. 
+    const defaultImageURL = 'https://images.unsplash.com/photo-1631551831518-b5b32d35f248';
+    
+    let style = {
+        /* set default style */
+        backgroundImage: `url(${defaultImageURL})`,
         backgroundSize:'cover',
+    }
+    
+    const image = useSelector(selectImage);
+
+    if (image !== undefined){ // image is undefined either on first render or when no image is returned from fetch. 
+        // don't run the following code if image doesn't contain image object.
+        const background = `url(${image.urls.regular})`;
+        style.backgroundImage = background;
     }
 
     return (
