fe5d6749ad21193bca70264b9b6761033c1f5c65
mikejguo290 <mikejguo290@gmail.com>
2021-09-28 18:19:32 +0100

    use a color mapping function to map four background colors to todo in todos.

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -5,7 +5,7 @@ import './Todo.css'
 
 function Todo(props){
     const [ activeTodo, setActiveTodo ] = useState(false); // allow Todo to track hover events, expressed with mouseEnter and mouseLeave.
-    const { id, text, isCompleted } = props;
+    const { id, text, isCompleted, bgColor } = props;
     const completionClass = isCompleted? 'completed': 'current';
     
     const dispatch = useDispatch();
@@ -26,11 +26,15 @@ function Todo(props){
         setActiveTodo(false);
     }
 
+    const styles = {
+        background: bgColor,
+    }
+
     return (
         <div className="todoContainer" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             {activeTodo && <button className={`deleteButton ${completionClass}`} onClick={handleDelete}>Remove</button>}
             {activeTodo && <button className={`completeButton ${completionClass}`} onClick={handleComplete}>Done</button>}
-            <article className={`todo ${completionClass}`}>
+            <article className={`todo ${completionClass}`} style={styles}>
                 <p>{text}</p>
             </article>
         </div>
--- src/features/Todos/index.js
+++ src/features/Todos/index.js
@@ -6,12 +6,45 @@ import './Todos.css';
 function TodosList(props){
 
     const todos = useSelector(selectTodos);
+    
+    const colors = [
+        'rgba(55, 98, 126, 0.6)',
+        'rgba(164, 21, 31, 0.6)',
+        'rgba(48, 52, 54, 0.6)',
+        'rgba(247, 127, 0, 0.6)'
+    ]; 
+    
+    // fn maps todos indices to a set of colors
+    const colorMapper = (index) => { 
+        //[0,1,2,3,4,5,6,7,8,9,10,11] => [0,1,2,3] [4,5,6,7] [8,9,10,11]. 
+        //the pattern is modulus of 4, the remainder when index is divided by 4.
+        
+        if(index%4===3){
+            return colors[3];
+        }else if(index%4===2){
+            return colors[2];
+        }else if(index%4===1){
+            return colors[1];
+        }else{
+            return colors[0];
+        }
+    }
+
+    // add background color attribute to each todo, have to create new object as todo cannot be extended in strict mode.
+
+    const todosExt = todos.map((todo, index)=>{
+        return {
+            ...todo,
+            bgColor: colorMapper(index),
+        }
+    });
+
 
     // either get all todos and filter later, or useSelectors conditionally 
-    const filteredItems = todos.filter(( todo )=> props.isCompleted === todo.isCompleted).map((todo)=>{
-        const {id, text, isCompleted } = todo;
+    const filteredItems = todosExt.filter(( todo )=> props.isCompleted === todo.isCompleted).map((todo)=>{
+        const {id, text, isCompleted , bgColor } = todo;
         return (
-            <li key={id}><Todo id={id} text={text} isCompleted={isCompleted}/></li>
+            <li key={id}><Todo id={id} text={text} isCompleted={isCompleted} bgColor={bgColor} /></li>
         )
     });
 
