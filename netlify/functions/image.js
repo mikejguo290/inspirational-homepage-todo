07533d878911fa4b1120a3c450a165952409eb2e
mikejguo290 <mikejguo290@gmail.com>
2021-10-05 00:40:41 +0100

    revoke exposed api secret and generate new ones for netlify. rename api secret environment variable.

--- netlify/functions/image.js
+++ netlify/functions/image.js
@@ -5,7 +5,7 @@ exports.handler = async function (event, context) {
     console.log(context);
     try {
         const { page, query } = event.queryStringParameters; 
-        const response = await axios.get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&page=${page}&query=${query}`);
+        const response = await axios.get(`https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_ACCESS_KEY}&page=${page}&query=${query}`);
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
