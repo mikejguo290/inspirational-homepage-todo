5d3f84fbf4955b81e6134ba99e07ee49f8550c99
mikejguo290 <mikejguo290@gmail.com>
2021-10-04 17:52:44 +0100

    use netlify function to access Open Weather api endpoint and grab api secret from .env file.

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -4,15 +4,15 @@ exports.handler = async function (event, context) {
     console.log(event);
     console.log(context);
     try {
-        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=68f0e305a7c7de98fc11d2bf9d32994c`);
+        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=${process.env.OPEN_WEATHER_API_SECRET}`);
         return {
             statusCode: 200,
-            body: JSON.stringify(response),
+            body: JSON.stringify(response.data),
         };
     } catch (err) {
         return {
             statusCode: 404,
-        body: err.toString(),
+            body: err.toString(),
         };
     }
 };
